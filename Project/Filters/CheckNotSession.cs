ce24c60a35c305336d9033d51988695479cb6733
Nguyễn Mạnh Tùng <63092029+FPT-NMTung@users.noreply.github.com>
2021-07-01 20:52:57 +0700

    Add filter session

---
+++ Project/Filters/CheckNotSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Filters {
+    public class CheckNotSession : ActionFilterAttribute {
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            var currentSession = HttpContext.Current.Session;
+
+            if ( currentSession["email"] != null &&
+                 currentSession["user_id"] != null &&
+                 currentSession["name"] != null ) {
+                filterContext.Result = new RedirectResult( "/" );
+            }
+        }
+    }
+}
