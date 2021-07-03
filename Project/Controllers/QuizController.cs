e4cf8ef2b366cfdc19819fdb8f502762bdebb832
DUONGMON2000 <86470329+DUONGMON2000@users.noreply.github.com>
2021-07-03 15:34:42 +0700

    htmlQuizCourse

---
+++ Project/Controllers/QuizController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class QuizController : Controller
+    {
+        // GET: Quiz
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Quiz()
+        {
+            return View();
+        }
+    }
+}
