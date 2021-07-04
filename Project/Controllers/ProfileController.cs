269118a0cd61719c81b66914ef249b09644095f1
mrlimfo <mrlimfo@gmail.com>
2021-07-05 00:26:22 +0700

    Profile Update
    load data from db to user profile

--- Project/Controllers/ProfileController.cs
+++ Project/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
 ﻿using Project.Filters;
+using Project.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,28 @@ namespace Project.Controllers
 {
     public class ProfileController : Controller
     {
+        private ProjectEntities db = new ProjectEntities();
         // GET: Profile
         [HttpGet]
         [CheckSession]
         public ActionResult Index()
         {
+            int id = -1;
+            if (Session["user_id"] != null)
+            {
+                int.TryParse(Session["user_id"].ToString(), out id);
+            }
+            var infor = from e in db.Users where e.UserID == id select e;
+            ViewBag.name = infor.ToList()[0].Name;
+            ViewBag.email = infor.ToList()[0].Email;
+            string gender = "Male";
+            if (infor.ToList()[0].Gender.Equals("False"))
+            {
+                gender = "Female";
+            }
+            ViewBag.gender = gender;
+            ViewBag.phone = infor.ToList()[0].PhoneNumber;
+            ViewBag.address = infor.ToList()[0].Address;
             return View();
         }
 
@@ -21,6 +39,22 @@ namespace Project.Controllers
         [CheckSession]
         public ActionResult EditProfile()
         {
+            int id = -1;
+            if (Session["user_id"] != null)
+            {
+                int.TryParse(Session["user_id"].ToString(), out id);
+            }
+            var infor = from e in db.Users where e.UserID == id select e;
+            ViewBag.name = infor.ToList()[0].Name;
+            ViewBag.email = infor.ToList()[0].Email;
+            string gender = "Male";
+            if (infor.ToList()[0].Gender.Equals("False"))
+            {
+                gender = "Female";
+            }
+            ViewBag.gender = gender;
+            ViewBag.phone = infor.ToList()[0].PhoneNumber;
+            ViewBag.address = infor.ToList()[0].Address;
             return View();
         }
     }
