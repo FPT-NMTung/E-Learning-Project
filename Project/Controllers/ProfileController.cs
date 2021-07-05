6126a34200b0fa4202b9e45c05d93756d4e04853
mrlimfo <mrlimfo@gmail.com>
2021-07-05 22:20:04 +0700

    update profile 05/7

--- Project/Controllers/ProfileController.cs
+++ Project/Controllers/ProfileController.cs
@@ -55,7 +55,37 @@ namespace Project.Controllers
             ViewBag.gender = gender;
             ViewBag.phone = infor.ToList()[0].PhoneNumber;
             ViewBag.address = infor.ToList()[0].Address;
-            return View();
+            return View(User);
+        }
+
+        [HttpGet]
+        [CheckSession]
+        public ActionResult EditUserProfile(string name, bool gender, string phone, string address)
+        {
+            //int id = -1;
+            //if (Session["user_id"] != null)
+            //{
+            //    int.TryParse(Session["user_id"].ToString(), out id);
+            //}
+            //try
+            //{
+            //    var update = db.Users.First(g => g.UserID == id);
+            //    if (TryUpdateModel(User))
+            //    {
+            //        update.Name = name;
+            //        update.Gender = gender;
+            //        update.PhoneNumber = phone;
+            //        update.Address = address;
+            //        db.SaveChanges();
+            //        return RedirectToAction("Index", "Profile");
+            //    }
+            //    return View(User);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return View();
+            //}
+            return RedirectToAction("Index");
         }
     }
 }
