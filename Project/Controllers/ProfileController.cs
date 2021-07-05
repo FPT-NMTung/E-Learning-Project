95ce369b9ea0beff8c278d2a82ff5aa103a96aab
mrlimfo <mrlimfo@gmail.com>
2021-07-06 00:27:56 +0700

    Profile update
    Edit infor, change pass without message/alert

--- Project/Controllers/ProfileController.cs
+++ Project/Controllers/ProfileController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Project.Controllers
 {
@@ -24,10 +25,10 @@ namespace Project.Controllers
             var infor = from e in db.Users where e.UserID == id select e;
             ViewBag.name = infor.ToList()[0].Name;
             ViewBag.email = infor.ToList()[0].Email;
-            string gender = "Male";
-            if (infor.ToList()[0].Gender.Equals("False"))
+            string gender = "Nam";
+            if (infor.ToList()[0].Gender == false)
             {
-                gender = "Female";
+                gender = "Nữ";
             }
             ViewBag.gender = gender;
             ViewBag.phone = infor.ToList()[0].PhoneNumber;
@@ -47,45 +48,64 @@ namespace Project.Controllers
             var infor = from e in db.Users where e.UserID == id select e;
             ViewBag.name = infor.ToList()[0].Name;
             ViewBag.email = infor.ToList()[0].Email;
-            string gender = "Male";
-            if (infor.ToList()[0].Gender.Equals("False"))
-            {
-                gender = "Female";
-            }
-            ViewBag.gender = gender;
             ViewBag.phone = infor.ToList()[0].PhoneNumber;
             ViewBag.address = infor.ToList()[0].Address;
-            return View(User);
+            ViewBag.genderMale = infor.ToList()[0].Gender == true ? "checked" : "";
+            ViewBag.genderFemale = infor.ToList()[0].Gender == false ? "checked" : "";
+            return View();
         }
 
-        [HttpGet]
+        [HttpPost]
         [CheckSession]
-        public ActionResult EditUserProfile(string name, bool gender, string phone, string address)
+        public ActionResult EditUserProfile(string name, string gender, string phone, string address)
         {
-            //int id = -1;
-            //if (Session["user_id"] != null)
-            //{
-            //    int.TryParse(Session["user_id"].ToString(), out id);
-            //}
-            //try
-            //{
-            //    var update = db.Users.First(g => g.UserID == id);
-            //    if (TryUpdateModel(User))
-            //    {
-            //        update.Name = name;
-            //        update.Gender = gender;
-            //        update.PhoneNumber = phone;
-            //        update.Address = address;
-            //        db.SaveChanges();
-            //        return RedirectToAction("Index", "Profile");
-            //    }
-            //    return View(User);
-            //}
-            //catch (Exception ex)
-            //{
-            //    return View();
-            //}
-            return RedirectToAction("Index");
+            int id = -1;
+            if (Session["user_id"] != null)
+            {
+                int.TryParse(Session["user_id"].ToString(), out id);
+            }
+            try
+            {
+                var update = db.Users.First(g => g.UserID == id);
+                update.Name = name;
+                update.Gender = gender == "male";
+                update.PhoneNumber = phone;
+                update.Address = address;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Profile");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("EditProfile", "Profile");
+            }
+        }
+
+        [HttpPost]
+        [CheckSession]
+        public ActionResult ChangePassword(string oldPass, string newPass, string rePass)
+        {
+            int id = -1;
+            if (Session["user_id"] != null)
+            {
+                int.TryParse(Session["user_id"].ToString(), out id);
+            }
+            try
+            {
+                var update = db.Users.First(g => g.UserID == id);
+                if (newPass.Equals(rePass) && oldPass.Equals(update.Password)){           
+                    update.Password = newPass;
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Profile");
+                }
+                else
+                {
+                    return RedirectToAction("EditProfile", "Profile");
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("EditProfile", "Profile");
+            }
         }
     }
 }
