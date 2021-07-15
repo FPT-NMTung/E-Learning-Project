5eb512230ad0c73f24522e676dcbf301f563e596
Nguyễn Mạnh Tùng <63092029+FPT-NMTung@users.noreply.github.com>
2021-07-16 01:39:50 +0700

    Done Course Page

--- Project/Controllers/CourseController.cs
+++ Project/Controllers/CourseController.cs
@@ -7,6 +7,11 @@ using System.Web.Mvc;
 using Project.Models;
 
 namespace Project.Controllers {
+    public class CourseList {
+        public List<Course> course { get; set; }
+        public List<Course> courseJoin { get; set; }
+        public List<int> IdVideoList { get; set; }
+    }
 
     public class CourseController : Controller {
         private ProjectEntities db = new ProjectEntities();
@@ -14,10 +19,52 @@ namespace Project.Controllers {
         // GET: Course
         [ HttpGet ]
         public ActionResult Index() {
-            var courses = from e in db.Courses select e;
-            var detaillession = from l in db.Lessions select l;
+            if ( Session["user_id"] == null ) {
+                var courses = from e in db.Courses select e;
 
-            return View( courses.ToList() );
+                CourseList list = new CourseList();
+                list.course = courses.ToList();
+
+                return View( list );
+            } else {
+                int userId = Convert.ToInt32( Session["user_id"].ToString() );
+
+                var courseJoin = (from cour in db.UserAndCourses
+                    join course in db.Courses on cour.CourseID equals course.CourseID
+                    where cour.UserID == userId
+                    select course).ToList();
+
+                List<int> courseIdList = new List<int>();
+
+                foreach ( var el in courseJoin ) {
+                    courseIdList.Add( el.CourseID );
+                }
+
+                var courses = (from e in db.Courses select e).ToList();
+
+                List<Course> tempCourses = new List<Course>();
+                List<int> tempList = new List<int>();
+
+                foreach ( var el in courses ) {
+                    var a = !courseIdList.Contains( el.CourseID );
+                    if ( !courseIdList.Contains( el.CourseID ) ) {
+                        tempCourses.Add( el );
+                    }
+                }
+
+                foreach ( var el in courseIdList ) {
+                    var temp
+                        = (from lesson in db.Lessions where lesson.CourseID == el select lesson).ToList()[0].LessionID;
+                    tempList.Add( temp );
+                }
+
+                CourseList list = new CourseList();
+                list.course = tempCourses;
+                list.courseJoin = courseJoin;
+                list.IdVideoList = tempList;
+
+                return View( list );
+            }
         }
 
         [ HttpGet ]
@@ -25,7 +72,7 @@ namespace Project.Controllers {
             var result = from e in db.Courses where e.CourseID.ToString() == id select e;
             //var lession = from l in db.Lessions where l.LessionID.ToString() == id select l;
             if ( result.ToList().Count == 0 ) {
-                return RedirectToAction( "Index" , "Course" );
+                return RedirectToAction( "Index", "Course" );
             }
 
             ViewBag.name = result.ToList()[0].Name;
@@ -33,13 +80,14 @@ namespace Project.Controllers {
             ViewBag.courseId = result.ToList()[0].CourseID;
             ViewBag.description = result.ToList()[0].Description;
 
-            //if (lession.ToList().Count == 0)
-            //{
-            //    return RedirectToAction("Index", "Course");
-            //}
-            //ViewBag.countvid = lession.ToList().Count[0].Countvid;
+            var temp
+                = (from lesson in db.Lessions where lesson.CourseID.ToString() == id select lesson).ToList();
 
+            if ( temp.ToList().Count == 0 ) {
+                return RedirectToAction( "Index", "Course" );
+            }
 
+            ViewBag.lessonId = temp[0].LessionID;
             return View();
         }
     }
