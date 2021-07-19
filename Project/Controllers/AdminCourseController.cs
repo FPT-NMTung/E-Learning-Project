6dd73c511a7eb03139abe8fffa310ac5f1211d57
mrlimfo <mrlimfo@gmail.com>
2021-07-20 04:20:29 +0700

    update course and delete course complete

--- Project/Controllers/AdminCourseController.cs
+++ Project/Controllers/AdminCourseController.cs
@@ -20,19 +20,33 @@ namespace Project.Controllers
         [CheckSessionAdmin]
         public ActionResult Index()
         {
+            //get admin name
+            int adminID = Convert.ToInt32(Session["admin_id"].ToString());
+
+            var admin = from a in db.Admins where a.AdminID == adminID select a;
+            ViewBag.adminName = admin.ToList()[0].Name;
+
             var listCourse = from c in db.Courses
                              select new ListCourse
                              {
                                  course = c
                              };
-
             return View(listCourse);
+
         }
 
+        [HttpGet]
+        [CheckSessionAdmin]
         public ActionResult CourseDetail(int courseID)
         {
+
+            int adminID = Convert.ToInt32(Session["admin_id"].ToString());
+
+            var admin = from a in db.Admins where a.AdminID == adminID select a;
+            ViewBag.adminName = admin.ToList()[0].Name;
+
             var selectedCourse = from c in db.Courses where c.CourseID == courseID select c;
-            if(selectedCourse.Count() == 0)
+            if (selectedCourse.Count() == 0)
             {
                 return RedirectToAction("Index", "Error");
             }
@@ -40,7 +54,54 @@ namespace Project.Controllers
             ViewBag.courseName = selectedCourse.ToList()[0].Name;
             ViewBag.description = selectedCourse.ToList()[0].Description;
             ViewBag.image = selectedCourse.ToList()[0].Image;
+
+            ViewBag.message = TempData["message"];
+            TempData.Clear();
             return View();
+        }
+
+        [HttpPost]
+        [CheckSessionAdmin]
+        public ActionResult UpdateCourse(int id, string name, string description, string image)
+        {
+            try
+            {
+                //update user profile
+                var update = db.Courses.First(g => g.CourseID == id);
+                update.Name = name.Trim();
+                update.Description = description.Trim();
+                update.Image = image.Trim();
+                db.SaveChanges();
+                TempData["message"] = true;
+                return RedirectToAction("CourseDetail", "AdminCourse", new { courseID = id });
+            }
+            catch (Exception)
+            {
+                TempData["message"] = false;
+                return RedirectToAction("CourseDetail", "AdminCourse", new { courseID = id });
+            }
+        }
+
+        [HttpPost]
+        [CheckSessionAdmin]
+        public ActionResult DeleteCourse(int id)
+        {
+            try
+            {
+                var remove = (from e in db.Courses
+                              where e.CourseID == id                             
+                              select e).FirstOrDefault();
+                if (remove != null)
+                {
+                    db.Courses.Remove(remove);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index", "AdminCourse");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("CourseDetail", "AdminCourse", new { courseID = id });
+            }
         }
     }
 }
