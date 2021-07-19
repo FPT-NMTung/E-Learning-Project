f91800e7a5990afc9f66dcb8b8774d35cf3feddd
Nguyễn Mạnh Tùng <63092029+FPT-NMTung@users.noreply.github.com>
2021-07-19 18:22:51 +0700

    Add login and filter page for admin

---
+++ Project/Controllers/AdminLoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Filters;
+using Project.Models;
+
+namespace Project.Controllers {
+    public class AdminLoginController : Controller {
+        private ProjectEntities db = new ProjectEntities();
+
+        // GET: AdminLogin
+        [ HttpGet ]
+        [ CheckNotSessionAdmin ]
+        public ActionResult Index() {
+            ViewBag.isLogged = TempData["isLogged"];
+            TempData.Clear();
+            return View();
+        }
+
+        [ HttpPost ]
+        [ CheckNotSessionAdmin ]
+        public ActionResult Login(string email, string password) {
+            var temp = (from admin in db.Admins where admin.Email == email && admin.Password == password select admin)
+                .ToList();
+
+            if ( temp.Count != 0 ) {
+                Session["admin_id"] = temp[0].AdminID;
+                return RedirectToAction( "Index", "Home" );
+            }
+
+            TempData["isLogged"] = false;
+
+            return RedirectToAction( "Index" );
+        }
+
+        [ HttpGet ]
+        [ CheckSessionAdmin ]
+        public ActionResult Logout() {
+            Session.Clear();
+
+            return RedirectToAction( "Index" );
+        }
+    }
+}
